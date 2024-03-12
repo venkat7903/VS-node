242ae5587b10702801af6358427c30c6e7b2984d
Venkat <raja.srigakollu1234@gmail.com>
2024-03-12 19:45:14 +0530

    added semicolon

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hi welcome')
-})
+});
 
 app.get('/date', (req, res) => {
     res.send(new Date())
-})
+});
 
-app.listen(3001)
+app.listen(3001);
 
-module.exports = app
+module.exports = app;
